bf7954c3caf51309251138b9e2eddb7cc0cb2311
Xenon <sofya.saxarova@mail.ru>
2026-02-25 09:47:46 +0300

    solves

--- task3/solution.sql
+++ task3/solution.sql
@@ -6,14 +6,14 @@ SET Session_Dur = (julianday(Session_End) - julianday(Session_Start)) * 24 * 60;
 
 CREATE VIEW v_users_activity AS
 SELECT
-    u.id AS User_ID,
-    u.first_name AS First_Name,
-    u.last_name AS Last_Name,
+    u.User_ID AS User_ID,
+    u.First_Name AS First_Name,
+    u.Last_Name AS Last_Name,
     COUNT(s.Log_ID) AS Num_Sessions,
     COALESCE(SUM(s.Session_Dur), 0) AS Total_Session_Time
 FROM users u
 LEFT JOIN server_logs s
-    ON u.id = s.User_ID
+    ON u.User_ID = s.User_ID
 GROUP BY
-    u.id, u.first_name, u.last_name
+    u.User_ID, u.First_Name, u.Last_Name
 ORDER BY Total_Session_Time DESC;

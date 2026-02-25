ec82beb001c9faaee3c62439fcb9da55d18a07f3
Xenon <sofya.saxarova@mail.ru>
2026-02-25 09:43:00 +0300

    solves

--- task3/solution.sql
+++ task3/solution.sql
@@ -4,7 +4,6 @@ ADD COLUMN Session_Dur REAL;
 UPDATE server_logs
 SET Session_Dur = (julianday(Session_End) - julianday(Session_Start)) * 24 * 60;
 
-
 CREATE VIEW v_users_activity AS
 SELECT
     u.id AS User_ID,

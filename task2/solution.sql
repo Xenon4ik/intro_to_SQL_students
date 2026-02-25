0940d870c077f984b3108fea82d8e767bcc9280d
Xenon <sofya.saxarova@mail.ru>
2026-02-25 09:34:21 +0300

    solves

--- task2/solution.sql
+++ task2/solution.sql
@@ -1,5 +1,5 @@
 CREATE VIEW v_region_sales_summary AS
 SELECT region, COUNT(*) AS Sales_count, SUM(CAST(Unit_Price AS REAL)* CAST(Quantity_Sold AS REAL) * (1 - CAST(Discount AS REAL))) AS Total_revenue
 FROM sales
-GROUP BY region;
+GROUP BY region
 ORDER BY Total_Revenue DESC;
